dc88649bb9218792d1d2c0a9623c5face55ae776
Matthew Deyo <mdeyo@mit.edu>
2018-04-29 17:36:01 -0700

    remove incorrect notes

--- src/update_goal_keyboard.py
+++ src/update_goal_keyboard.py
@@ -3,40 +3,13 @@ import roslib
 roslib.load_manifest('teleop_twist_keyboard')
 import rospy
 
-from std_msgs.msg import Empty, String, Float64, Bool
+from std_s.msg import Empty, String, Float64, Bool
 from geometry_msgs.msg import TransformStamped
 
 import sys
 import select
 import termios
 import tty
-
-
-msg = """
-Reading from the keyboard  and Publishing to Twist!
----------------------------
-Moving around:
-   u    i    o
-   j    k    l
-   m    ,    .
-
-For Holonomic mode (strafing), hold down the shift key:
----------------------------
-   U    I    O
-   J    K    L
-   M    <    >
-
-t : up (+z)
-b : down (-z)
-
-anything else : stop
-
-q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
-
-CTRL-C to quit
-"""
 
 
 def getKey():
